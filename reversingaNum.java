e49b410ce6eec19481d4667f7d89ef4f19c22d20
Arunachalamm18 <arunachalama25@gmail.com>
2022-10-17 11:21:20 +0530

    variable name change

--- reversingaNum.java
+++ reversingaNum.java
@@ -4,15 +4,15 @@ public class reversingaNum {
 
 	public static void main(String[] args) {
 		
-		int num=432;
-		int rev=0;
+		int number=432;
+		int revtemp=0;
 		int temp=num;
 		while(temp>0)
 		{
-		rev=(rev*10)+temp%10;
+		revtemp=(revtemp*10)+temp%10;
 		temp/=10;
 		}
-		System.out.println(rev);
+		System.out.println(revtemp);
 	}
 	
 
